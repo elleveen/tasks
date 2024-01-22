b8de5143564e862838471da9b9d261d71f6b2e75
elleveen <eleonoramanushkova@yandex.ru>
2024-01-22 16:22:49 +0700

    remake Task2

--- projects/Task_2.js
+++ projects/Task_2.js
@@ -1,6 +1,12 @@
 let str = 'hello alexandr!';
-let replacedStr = str.replace('hello', 'hhhhh').replace('alexandr!', 'aaaaaaaaa');
-console.log(replacedStr);
+let words = str.split(' ');
+let result = words.map(word => word[0].repeat(word.length));
+let finalStr = result.join(' ');
+console.log(finalStr);
+
+// let str = 'hello alexandr!';
+// let replacedStr = str.replace('hello', 'hhhhh').replace('alexandr!', 'aaaaaaaaa');
+// console.log(replacedStr);
 
 // или)))
 
